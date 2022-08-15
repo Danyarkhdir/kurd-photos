2f0b1c0c010576196546cc955366e0b90c6313b8
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-15 22:54:17 +0300

    refactor: some changes to navbar on mobile version

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import "./App.css";
-import Navbar from "./components/Navbar";
+import MainLayout from "./layouts/MainLayout";
 function App() {
   return (
     <div className="bg-primary-500 w-full h-screen">
-      <Navbar />
+      <MainLayout />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,13 +18,14 @@ export default function Navbar() {
             <AiFillCamera />
           </span>
           Kurd Photos
+          <div
+            onClick={() => setOpen(!open)}
+            className="text-4xl absolute right-8  cursor-pointer md:hidden "
+          >
+            {open ? <ImMenu4 /> : <ImMenu3 />}
+          </div>
         </div>
-        <div
-          onClick={() => setOpen(!open)}
-          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden"
-        >
-          {open ? <ImMenu4 /> : <ImMenu3 />}
-        </div>
+
         <ul
           className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
             open ? "top-16 " : "top-[-490px]"
@@ -38,8 +39,8 @@ export default function Navbar() {
               {link.name}
             </li>
           ))}
-          <Button key="login">Log in</Button>
-          <Button key="register">Get Started</Button>
+          <Button>Log in</Button>
+          <Button>Get Started</Button>
         </ul>
       </div>
     </div>
