d0a37b55c747caac54835bc319cdec3395d3428f
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-15 23:50:58 +0300

    config: add main layout && style: re-style some components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,8 @@
 export default function Button(props) {
   return (
-    <button className="bg-primary-100 md:mx-0 mx-1 text-white font-[Poppins] py-2 px-6 rounded md:ml-8 hover:bg-indigo-400 duration-500 ">
+    <button
+      className={`${props.color} md:mx-0 mr-1 text-white font-bold  py-2 px-6 rounded md:ml-8 ${props.onHover} duration-500`}
+    >
       {props.children}
     </button>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { AiFillCamera } from "react-icons/ai";
 import Button from "./Button";
-import { ImMenu3, ImMenu4 } from "react-icons/im";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import { useState } from "react";
 export default function Navbar() {
   const links = [
@@ -12,35 +12,42 @@ export default function Navbar() {
   let [open, setOpen] = useState(false);
   return (
     <div className="shadow-md w-full fixed top-0 left-0 ">
-      <div className="md:flex md:items-center justify-between bg-white py-4 md:px-10 px-7">
-        <div className="font-bold text-2xl cursor-pointer flex items-center font-[Poppins] ">
-          <span className="text-3xl  text-primary-400 pr-1">
+      <div className="md:flex md:items-center justify-between bg-primary-400 py-4 md:px-10 px-7">
+        <div className="font-bold text-2xl cursor-pointer flex items-center bg-primary-400 ">
+          <span className="text-3xl  text-white  pr-1">
             <AiFillCamera />
           </span>
-          Kurd Photos
+          <span className="font-extraBold text-white  fontFamily-nunito">
+            KrPics
+          </span>
+
           <div
             onClick={() => setOpen(!open)}
-            className="text-4xl absolute right-8  cursor-pointer md:hidden "
+            className="text-3xl absolute right-8 text-white  cursor-pointer md:hidden "
           >
-            {open ? <ImMenu4 /> : <ImMenu3 />}
+            {open ? <AiOutlineClose /> : <AiOutlineMenu />}
           </div>
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 pb-12 absolute md:static bg-white md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${
+          className={`md:flex md:items-center   md:pb-0 pb-12 absolute md:static bg-black md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ${
             open ? "top-16 " : "top-[-490px]"
           }`}
         >
           {links.map((link) => (
             <li
               key={link.name}
-              className="md:ml-8 text-xl cursor-pointer text-primary-400 hover:text-primary-200 duration-500 md:my-0 my-7  "
+              className="md:ml-8 text-xl cursor-pointer text-white hover:text-gray-800 duration-500 md:my-0 my-7  "
             >
               {link.name}
             </li>
           ))}
-          <Button>Log in</Button>
-          <Button>Get Started</Button>
+          <Button color="bg-sky-800" onHover="hover:bg-sky-600">
+            Log in
+          </Button>
+          <Button color="bg-green-600" onHover="hover:bg-green-700">
+            Get Started
+          </Button>
         </ul>
       </div>
     </div>
