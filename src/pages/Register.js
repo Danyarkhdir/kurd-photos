8550a52b0dcfd72ad162ea02ab8d96a5f769b898
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-16 21:35:04 +0300

    style: add breakpoint

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -7,7 +7,7 @@ export default function Register() {
       <div className="md:w-4/12 xl:w-5/12  h-64 bg-center  bg-black    md:h-screen bg-gray-500 bg-cover bg-no-repeat bg-[url('https://images.unsplash.com/photo-1627987469780-fc77d8c9a586?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80')]">
         <div className="px-2 py-4 md:px-6 md:py-6 lg:px-8  lg:py-8 xl:px-12 xl:py-10  ">
           <MdCamera size={"50px"} color={"white"} />
-          <h1 className="md:text-4xl md:mt-80 text-4xl mt-28 xl:text-5xl lg:mt-80 xl:mt-80    text-2xl font-bold text-white">
+          <h1 className="md:text-4xl md:mt-80 text-4xl mt-28 sm:mt-32 xl:text-5xl lg:mt-80 xl:mt-80    text-2xl font-bold text-white">
             Welcome to KrPics
           </h1>
         </div>
