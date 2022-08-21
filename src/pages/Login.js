ca588c9af424eb36bd7aeba69037ca00a07a5967
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-21 17:23:17 +0300

    hotfix: add i18n translation to login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,11 @@ import { AiOutlineCamera } from "react-icons/ai";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { useDispatch } from "react-redux/es/exports";
+import { useTranslation } from "react-i18next";
 import { login } from "../features/user/authSlice";
 
 export default function Login() {
+  const { t } = useTranslation("common");
   const navigate = useNavigate();
   const [user, setUser] = useState("");
   const dispatch = useDispatch();
@@ -18,8 +20,8 @@ export default function Login() {
     <div className="md:w-6/12 h-screen w-full mx-auto ">
       <div className="text-black  p-1 flex flex-col items-center">
         <AiOutlineCamera className="text-black text-8xl " />
-        <h1 className="text-5xl mt-2 font-extrabold">Login</h1>
-        <h3 className="mt-6  text-l">Welcome Back.</h3>
+        <h1 className="text-5xl mt-2 font-extrabold">{t("login.login")}</h1>
+        <h3 className="mt-6  text-l">{t("login.welcome")}</h3>
       </div>
       <form
         onSubmit={onSubmit}
@@ -28,7 +30,7 @@ export default function Login() {
       >
         <label className="text-xl" htmlFor="username">
           <span className="text-xl after:content-['*'] after:ml-0.5 after:text-red-500 block  text-black">
-            Username
+            {t("login.username")}
           </span>
         </label>
         <input
@@ -37,16 +39,16 @@ export default function Login() {
           className="h-11 p-2 mt-1 border border-gray-600 rounded focus:outline-none focus:ring-1 focus:ring-black"
           id="username"
           name="username"
-          placeholder="username"
+          placeholder={t("login.username").toLowerCase()}
         />
         <div className="flex justify-between mt-6">
           <label className="text-xl" htmlFor="password">
             <span className="text-xl after:content-['*'] after:ml-0.5 after:text-red-500 block  text-black">
-              Password
+              {t("login.password")}
             </span>
           </label>
           <span className="text-gray-600 cursor-pointer hover:text-black mr-1 ">
-            Forgot your password?{" "}
+            {t("login.forgotPassword")}
           </span>
         </div>
         <input
@@ -55,7 +57,7 @@ export default function Login() {
           type="password"
           name="pass"
           id="pass"
-          placeholder="password"
+          placeholder={t("login.password").toLowerCase()}
         />
         <input
           type="submit"
@@ -65,12 +67,12 @@ export default function Login() {
       </form>
       <div className="border border-gray-500 mx-2 py-5 text-center mt-6 rounded-sm text-l">
         <p>
-          Don’t have an account?{" "}
+          {t("login.dontHaveAccount")}
           <Link
             className="underline  text-gray-600 hover:text-black"
             to="/register"
           >
-            Join KrPics
+            {t("login.join")} KrPics
           </Link>
         </p>
       </div>
