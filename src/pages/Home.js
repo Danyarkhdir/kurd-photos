90895a6b34289092c6177c0c1f2c7f6f54834672
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-24 02:51:50 +0300

    refactor: add (previous, next) to HomePage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Loading from "../components/Loading";
+import { render } from "@testing-library/react";
 export default function Home() {
   const [data, setData] = useState();
+  const [page, setPage] = useState(1);
+  console.log(page);
+
   useEffect(() => {
     axios
       .get(
-        `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&per_page=50`
+        `https://api.unsplash.com/photos?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&page=${page}&per_page=50`
       )
       .then((response) => {
         setData(response.data);
@@ -14,23 +18,52 @@ export default function Home() {
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [page]);
   return (
-    <div className="py-20  md:mt-32 flex flex-wrap gap-5 justify-center bg-primary-500">
-      {!data ? (
-        <Loading />
-      ) : (
-        data.map((item) => {
-          return (
-            <img
-              className="cursor-pointer xs:w-full  md:w-[22rem] lg:w-[28rem] xl:w-[26rem] object-cover "
-              key={item.id}
-              src={item.urls.regular}
-              alt="something"
-            />
-          );
-        })
-      )}
+    <div className="w-full  flex flex-col bg-primary-500 pt-20 pb-10">
+      <div className=" md:mt-32 flex flex-wrap gap-5 justify-center ">
+        {!data ? (
+          <Loading />
+        ) : (
+          data.map((item) => {
+            return (
+              <img
+                className="cursor-pointer xs:w-full  md:w-[22rem] lg:w-[28rem] xl:w-[26rem] object-cover "
+                key={item.id}
+                src={item.urls.regular}
+                alt="something"
+              />
+            );
+          })
+        )}
+      </div>
+      <div className="flex justify-center pt-10">
+        {/*  Previous Button */}
+        <p
+          onClick={() => {
+            page >= 1 ? setPage(page - 1) : setPage(page);
+            render();
+          }}
+          className={`cursor-pointer disabled ${
+            page === 1 ? "hidden" : "inline-flex items-center"
+          }   py-2 px-4 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+        >
+          Previous
+        </p>
+
+        {/* Next Button */}
+        <p
+          onClick={() => {
+            page <= 1000 ? setPage(page + 1) : setPage(page);
+            render();
+          }}
+          className={`cursor-pointer ${
+            page === 1000 ? "hidden" : "inline-flex items-center"
+          }    py-2 px-4 ml-3 text-sm font-medium text-gray-500 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+        >
+          Next
+        </p>
+      </div>
     </div>
   );
 }
