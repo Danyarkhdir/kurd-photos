b38c44992e5880b7ada9dc7b585df82a1522ff45
Danyar Khdir <daniyar.khdir@gmail.com>
2022-08-30 22:17:47 +0300

    .

--- src/components/SmSearchBar.js
+++ src/components/SmSearchBar.js
@@ -10,7 +10,10 @@ export default function SmSearchBar() {
       className="w-7/12 md:hidden "
     >
       <div className="flex  items-center justify-around xs:mx-5 font-light xs:text-base sm:text-lg   bg-gray-200  px-2 hover:bg-white  rounded-3xl">
-        <button type="submit" className=" bg-transparent  w-1/12  ">
+        <button
+          type="submit"
+          className=" bg-transparent  xs:w-2/12 sm:w-1/12  xs:mx-1 sm:mx-0"
+        >
           <AiOutlineSearch
             size={22}
             className="text-gray-500 hover:text-black"
@@ -19,7 +22,7 @@ export default function SmSearchBar() {
         <input
           placeholder={t("navbar.search")}
           type="search"
-          className=" bg-transparent w-11/12    outline-none py-1 xs:px-3 sm:px-1"
+          className=" bg-transparent xs:w-10/12 sm:11/12    outline-none py-1 xs:px-3 sm:px-1"
         />
       </div>
     </form>
